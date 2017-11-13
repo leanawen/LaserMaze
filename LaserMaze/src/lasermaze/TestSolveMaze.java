f09b30a8902fcd06c6a2c61cb6519dad48e1990e
Jingran <jingranwen@gmail.com>
2017-11-12 17:43:18 -0700

    Add test case of an empty file.

--- LaserMaze/src/lasermaze/TestSolveMaze.java
+++ LaserMaze/src/lasermaze/TestSolveMaze.java
@@ -71,5 +71,35 @@ public class TestSolveMaze {
 		
 		
 	}
+	
+	@Test
+	public void testMaze3() throws IOException {
+		String inputfile = "c:/test/inputtest4.txt";
+		MyMaze maze = new MyMaze();
+		MazeParameters parameters;
+		try {
+			parameters = new MazeParameters();
+			parameters.getParametersFromFile(inputfile);
+			int gridX = parameters.getGridSizeX();
+			int gridY = parameters.getGridSizeY();
+			MyMirror[][] mirrors = parameters.getMirrors();
+			Status startStatus = parameters.getStartStatus();
+			
+			maze.solveMaze(startStatus, mirrors, gridX, gridY);
+			System.out.println(maze.getLength());
+			System.out.println(maze.getFinalCoordinationX() + " " + maze.getFinalCoordinationY());
+			System.out.println();
+			
+			assertEquals(maze.getLength(), -1);
+			assertEquals(maze.getFinalCoordinationX(), Integer.MIN_VALUE);
+			assertEquals(maze.getFinalCoordinationY(), Integer.MIN_VALUE);
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
 
 }
